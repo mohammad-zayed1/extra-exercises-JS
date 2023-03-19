561dfc87a6453859cab8c8e700678a75dc51cf4f
mohammad zayed <mohammadzayed193@gmail.com>
2023-03-19 16:45:49 +0300

    add new exercise

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ console.log(7 <= 8);
 console.log(Math.ceil(2.5) - Math.floor(2.5));
 
 
-// Q-2 :
+ // Q-2 :
 
 const readNumber = prompt('Enter a number');
 alert(readNumber);
@@ -59,20 +59,20 @@ let n1 = Number(prompt('Enter a number 1'));
 let n2 = Number(prompt('Enter a number 2'));
 alert('the larger number is ' + Math.max(n1, n2));
 
-// Q-5 :
-let x = prompt('Enter a number 1');
-let y = prompt('Enter a number 2');
+// // Q-5 :
 
+let x = Number(prompt('Enter a number 1'));
+let y = Number(prompt('Enter a number 2'));
 alert(x + y);
 
-// problem : if the user enter 1 and 2 the sum of these value = 12 not 3
+//  problem : if the user enter 1 and 2 the sum of these value = 12 not 3
 
 
-// Q-6 :
+ // Q-6 :
 let num = prompt('Enter a number between 1-9');
 
 switch (num) {
-    case '1': alert('ONE');  break;
+    case '1': alert('ONE'); break; 
     case '2': alert('TWO');  break;
     case '3': alert('THREE');  break;
     case '4': alert('FOUR');  break;
@@ -85,3 +85,76 @@ switch (num) {
     default:alert('PLEASE TRY AGAIN , THE NUMBER IS NONE OF THE RANGE');
         break;
 }
+
+
+// Q-7
+
+for (let i = 0; i <= 5 ; i++) {
+    alert(i) ; 
+}
+
+//Q-8
+
+let numbers = "";
+let i = 0;
+while(i <= 5){
+    numbers += i + " ";
+    i++;
+} 
+
+// Q-9 
+
+let nums = "";
+for (let i = 1; i <= 20; i++) {
+    if( i % 3 == 0){
+        nums += i + " ";
+    }else{
+        continue;
+    }
+    
+}
+alert(nums);
+
+// Q-10
+
+let num = prompt("plz , enter number between 0 - 100");
+
+    while((num > 100 || num < 0) || num == '' ){
+        num = prompt('Error , enter valid number');
+    }
+
+// Q-11
+
+
+let num = prompt("plz , enter number between 0 - 100");
+if(isNaN(num) != true){
+    while((num > 100 || num < 0) || num == '' ){
+        num = prompt('Error , enter valid number');
+    }
+}else{
+    alert("Error")
+}
+
+// Q-12
+
+let intNumber = prompt("enter integer number");
+let sum = 0;
+for (let i = 0; i <= intNumber; i++) {
+    
+    sum +=i;
+}
+alert(sum);
+
+// Q-13
+
+let intNum = prompt("enter integer number");
+let sumAvg = 0;
+let i;
+for ( i = 0; i <= intNum; i++) {
+    
+    sumAvg +=i;
+}
+alert('the avg = ' + (sumAvg / i) );
+
+
+
---
+++ whileLoop.js
@@ -0,0 +1,43 @@
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// syntax
+
+// while (condition) {
+
+//    // code block to be executed
+
+// }
+
+
+
+
+// Example 
+
+let i = 1;
+let age = prompt('plz enter your age');
+while (age > 20) {
+    age = prompt('dont lie , enter your real age');    
+}
+alert('get out');
+
+
