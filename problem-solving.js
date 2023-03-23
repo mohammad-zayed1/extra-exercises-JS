270fe206dffa628bf1f57f3ff632f027a1915286
mohammad zayed <mohammadzayed193@gmail.com>
2023-03-23 15:20:49 +0300

    add new 3 question solution [5 , 6 , 7]

--- problem-solving.js
+++ problem-solving.js
@@ -51,3 +51,49 @@
 //     return avg;
 //   }
   
+
+
+
+// Q-5
+
+// let arr = [1 , 2, 3 , 4, 5, 6 , 7 , 8 , 9 , 10];
+// let newArr = [];
+// function squareNumber(arr){
+// //  let newArr = arr.map((value)=> Math.pow(value , 2)); // return new array
+// // arr.forEach( (value) => newArr.push(Math.pow(value , 2))); // must push every value to new array
+//  console.log(newArr);
+// }
+// squareNumber(arr);
+
+
+// Q-6 
+// let arr = [1 , 2, 3 ,"mohammad", 4, 5, 6 , 7 ,"#$$", 8 , 9 , 10 ];
+
+// function oddEvenNumbers(arr){
+//  let newArr = arr.map((item)=>{
+//     if(typeof item === 'number'){
+//         if(item % 2 === 0) return 'even'
+//         else if(item % 2 !== 0) return 'odd'
+//     }else{
+//         return 'N/A'
+//     }
+    
+//  })
+//  console.log(newArr);
+// }
+// oddEvenNumbers(arr);
+
+
+// Q-7
+// let arr = [1 , 2, 3 , 4, 5, 6 , 7 , 8 , 9 , 10 , 11 , 12 , 13 , 14 , 15 , 30];
+// function fizzbuzz(arr){
+// let newArr = arr.map((num)=>{
+//     if(num % 3 === 0 && num % 5 === 0) return 'Fizz Buzz'
+//     else if(num % 5 === 0) return 'Buzz'
+//     else if(num % 3 === 0) return 'Fizz'
+//     else return num;
+// })
+// console.log(newArr);
+// }
+
+// fizzbuzz(arr);
